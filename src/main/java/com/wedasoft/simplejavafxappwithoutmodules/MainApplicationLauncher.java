9ab9fd2f8073082936e526687609fc57e2313eda
David Weber <weberdavid91@gmail.com>
2022-12-22 15:25:11 +0100

    feat: initial commit

---
+++ src/main/java/com/wedasoft/simplejavafxappwithoutmodules/MainApplicationLauncher.java
@@ -0,0 +1,9 @@
+package com.wedasoft.simplejavafxappwithoutmodules;
+
+public class MainApplicationLauncher {
+
+    public static void main(String[] args) {
+        MainApplication.main2(args);
+    }
+
+}
